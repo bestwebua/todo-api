5aa0c1c01ec5bbfccb7a19608d4385b846f0449d
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-10-22 16:54:04 +0300

    Complete ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,38 @@
 class ProjectsController < ApplicationController
+  before_action :init_project, only: %i[show update destroy]
+
+  def index
+    @projects = Project.all
+    json_response(@projects)
+  end
+
+  def create
+    @project = Project.create!(project_params)
+    json_response(@project, :created)
+  end
+
+  def show
+    json_response(@project)
+  end
+
+  def update
+    @project.update(project_params)
+    head :no_content
+  end
+
+
+  def destroy
+    @project.destroy
+    head :no_content
+  end
+
+  private
+
+    def project_params
+      params.permit(:title, :created_by)
+    end
+
+    def init_project
+      @project = Project.find(params[:id])
+    end
 end
