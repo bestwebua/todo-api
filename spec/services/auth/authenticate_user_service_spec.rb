2533c51c71649b7e8bbced28cdc8908af5a3b579
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-10-28 22:13:03 +0200

    Update AuthenticateUserService.call tests

--- spec/services/auth/authenticate_user_service_spec.rb
+++ spec/services/auth/authenticate_user_service_spec.rb
@@ -2,13 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Auth::AuthenticateUserService do
   let(:user)                 { create :user, sign_out: true }
-  subject(:valid_auth_obj)   { described_class.call(email: user.email, password: user.password) }
   subject(:invalid_auth_obj) { described_class.call(email: '', password: '') }
+
+  subject(:valid_auth_obj) do
+    described_class.call(email: user.email, password: user.password)
+    user.reload
+  end
 
   describe '.call' do
     context 'valid credentials' do
-      specify { expect { valid_auth_obj }.to change(user, :sign_out) }
-      specify { expect(valid_auth_obj).not_to be_nil }
+      it 'user sign_out status should be false' do
+        expect { valid_auth_obj }.to change(user, :sign_out)
+      end
+
+      it 'returns an auth token' do
+        expect(valid_auth_obj).not_to be_nil
+      end
     end
 
     context 'invalid credentials' do
