9a56d42340fe2c527d8619dacde4cdf5500967ce
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-11-01 16:31:09 +0200

    Erase unused methods

--- app/services/auth/message_service.rb
+++ app/services/auth/message_service.rb
@@ -1,10 +1,6 @@
 module Auth
   class MessageService
     class << self
-      def not_found(record = 'record')
-        "#{record} not found."
-      end
-
       def invalid_credentials
         'Invalid credentials'
       end
@@ -17,20 +13,12 @@ module Auth
         'Missing token'
       end
 
-      def unauthorized
-        'Unauthorized request'
-      end
-
       def account_created
         'Account created successfully'
       end
 
       def account_not_created
         'Account could not be created'
-      end
-
-      def expired_token
-        'Your token has expired. Please login to continue.'
       end
 
       def password_error
