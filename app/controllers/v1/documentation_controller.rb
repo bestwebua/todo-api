a6f0cd4abde994230e0e0d9ff1edc44419811f6c
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-11-02 15:27:22 +0200

    Fix typo

--- app/controllers/v1/documentation_controller.rb
+++ app/controllers/v1/documentation_controller.rb
@@ -1,7 +1,7 @@
 module V1
   class DocumentationController < ActionController::Base
     def index
-      render file: Api::DocSelectorService.call('1'), layout: false
+      render file: Api::DocSelectorService.call(self), layout: false
       rescue ActionView::MissingTemplate => error
         raise ActionController::RoutingError.new('Documentation with current version not found.')
     end
